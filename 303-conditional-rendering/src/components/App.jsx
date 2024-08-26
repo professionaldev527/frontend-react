26f2db5be7ad7eb545742165d6427406565b45b1
professionaldev527 <professionaldev527@gmail.com>
2024-08-26 16:15:26 +0530

    303-conditional-rendering
    React Conditional Rendering with the Ternary Operator & AND Operator

---
+++ 303-conditional-rendering/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+function App() {
+  return (
+    <div className="container">
+      {/*Ternary Operator*/}
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {/*AND Operator*/}
+      {currentTime > 12 && <h1>Why are you still working?</h1>}
+    </div>
+  );
+}
+
+export default App;
