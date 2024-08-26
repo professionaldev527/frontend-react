ce8e3af06b93ea527931132d9fdf0645def7bbbb
professionaldev527 <professionaldev527@gmail.com>
2024-08-26 16:02:45 +0530

    293-keeper-app-part-1
    Keeper App Project - To do app with react part 1

---
+++ 293-keeper-app-part-1/src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../../public/styles.css';
+
+function Note() {
+    return (
+        <note>
+         <h1>This is the note title</h1>
+        <p>This is the note content</p>
+        </note>
+    );
+}
+
+export default Note;
